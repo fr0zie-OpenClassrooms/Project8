2ca6b808f76bda972097eb9d504be3050b90d8e9
Axel Rayet <axel.rayet@outlook.com>
2021-12-09 13:01:31 +0100

    Final details

--- home/views.py
+++ home/views.py
@@ -5,5 +5,9 @@ def index(request):
     return render(request, "home/index.html")
 
 
+def legal(request):
+    return render(request, "home/legal.html")
+
+
 def not_found(request, exception=None):
     return render(request, "home/404.html")
